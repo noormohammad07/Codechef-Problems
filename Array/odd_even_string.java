23e1163cd2e5cdeaf08eff1c37fbc7f1e7872438
noormohammad07 <noormahammad3203@gmail.com>
2023-01-19 20:55:16 +0530

    odd_even_string

---
+++ Array/odd_even_string.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextInt();
+            }
+            int count=0;
+            for(int i=0; i<n; i++){
+                if(arr[i]==0){
+                    count++;
+                }
+            }
+            if(count%2==0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+
+        }
+    }
+}
